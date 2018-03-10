1294f8a065e8fe68ee85455294ff1960cfa541e5
Venkata Neeraja Upadrasta <venkata.upadrasta@st.ovgu.de>
2018-03-10 11:14:23 +0100

    Customized nav bar

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,14 +5,20 @@ import { Link } from 'react-router-dom'
 // between routes.
 const Header = () => (
   <header>
-    <nav>
-      <ul>
-        <li><Link to='/'>Home</Link></li>
-        <li><Link to='/Chordfinal'>Chord</Link></li>
-        <li><Link to='/heatmap'>HeatMap</Link></li>
-        <li><Link to='/Bar'>Bar</Link></li>
-        <li><Link to='/Pie'>Pie</Link></li>
-        </ul>
+    <nav class="navbar navbar-inverse">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="#">Relational Visualization</a>
+    </div>
+    <ul class="nav navbar-nav">
+    <li><Link to='/'>Home</Link></li>
+    <li><Link to='/Chordfinal'>Chord</Link></li>
+    <li><Link to='/heatmap'>HeatMap</Link></li>
+    <li><Link to='/Bar'>Bar</Link></li>
+    <li><Link to='/Pie'>Pie</Link></li>
+    </ul>
+  </div>
+    
     </nav>
   </header>
 )
--- src/components/Home.js
+++ src/components/Home.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Home = () => (
   <div>
-    <h1>Relational Visualization</h1>
+   
   </div>
 )
 
