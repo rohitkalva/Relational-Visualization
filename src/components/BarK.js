d9221be8e3c7048e466ce3ebaba8425dd795eefa
Rohit K <rohitkalva@gmail.com>
2018-04-25 14:51:51 +0200

    Added rank and category dropdown for Bar chart

--- src/components/BarK.js
+++ src/components/BarK.js
@@ -1,6 +1,18 @@
 import React from 'react';
 import axios from 'axios';
 import { Bar} from 'react-chartjs-2';
+import {
+  Sidebar,
+  Segment,
+  Dropdown,
+  Button,
+  Menu,
+  Icon
+} from "semantic-ui-react";
+import "../css/appone.css";
+import "../css/bootstrap.min.css";
+import "../css/style.css";
+import "../css/bar.css";
 
 class BarK extends React.Component {
 
@@ -8,8 +20,17 @@ class BarK extends React.Component {
     super(props);
     this.state = {
       chartData: {},
+      master: {},
+      categoryList: [],
+      selectedCategory: "",
     }
+
+    this.importJSON = this.importJSON.bind(this);
+    this.updateList = this.updateList.bind(this);
   }
+
+  toggleVisibility = () => this.setState({ visible: !this.state.visible });
+
    static defaultProps = {
         displayTitle:true,
         displayLegend:true,
@@ -17,39 +38,144 @@ class BarK extends React.Component {
         
     }
   componentDidMount() {
-     axios('http://localhost:8080/visualization/barpie/keyword')
-      .then((response) => {
- 
-        const { keywordData } = response.data;
+    this.importJSON()
+  }
+
+  importJSON() {
+    const categories = {};
+    const master = {};
+    const categoryList = [];
+
+    return fetch("http://localhost:8080/visualization/barpie/keyword")
+       .then(response => response.json())
+        .then(responseJson => {
+          this.setState(
+            {
+              TAXONOMY_DATA: responseJson.keywordData
+            },
+            () => {
+              this.state.TAXONOMY_DATA.forEach(d => {
+                const keyword = d.category;
+
+                categories[keyword] = true;
+                if (!master[keyword]) {
+                  master[keyword] = [];
+                }
+
+                master[keyword].push(d);
+              });
+
+              // add ranks/categories to list
+              
+              for (let key in categories) {
+                if (categories.hasOwnProperty(key)) {
+                  categoryList.push(key);
+                }
+              }
+             console.log("categoryList", categoryList);
+              //console.log('master', master)
+
+              this.setState(
+                {
+                  categoryList,
+                  master,
+                  selectedCategory: categoryList[0]
+                  // selectedRank: "kingdom",
+                  // selectedCategory: "Technical term",
+                },
+                () => {
+                  this.updateList();
+                }
+              );
+            }
+          );
+        })
+  }
+
+  updateList(){
+    const { master, selectedCategory } = this.state;
+    console.log(`master[${selectedCategory}]`);
+    const Data = master[selectedCategory];
+
         const chartData = {
-          labels: keywordData.map(k => k.keywordName),
+          labels: Data.map(k => k.keywordName),
           datasets: [
             {
               label: 'SpectraCount',
-              data: keywordData.map(d => d.noOfSpectra),
+              data: Data.map(d => d.noOfSpectra),
               backgroundColor: '#1c0549', 
             },
             {
               label: 'ProteinCount',
-              data: keywordData.map(p => p.noOfProtein),
+              data: Data.map(p => p.noOfProtein),
               backgroundColor: '#ff005a',
             },
             {
               label: 'PeptideCount',
-              data: keywordData.map(e => e.noOfPeptide),
+              data: Data.map(e => e.noOfPeptide),
               backgroundColor:  '#65cde3',
             }
           ]
         }
         this.setState({ chartData });
-      });
   }
   
 
 render() {
+  const {
+    visible,
+    categoryList,
+    selectedCategory
+  } = this.state;
+
   return (
-    <div className="App">
-        <Bar
+    <div className="container">
+    <Sidebar.Pushable as={Segment} >
+          <Button
+            onClick={this.toggleVisibility}
+            icon
+            className="navbar-toggle"
+          >
+            <Icon name="align justify" />
+          </Button>
+          <Sidebar
+            as={Menu}
+            animation="overlay"
+            width="very wide"
+            direction="right"
+            visible={visible}
+            icon="labeled"
+            vertical
+            inverted
+          >
+            <Icon
+              className="close"
+              size="large"
+              onClick={this.toggleVisibility}
+            />
+            <div className="row mt-50" />
+
+            <div className="row mt-20">
+            <div className="col-sm-6">
+                <Dropdown
+                  placeholder="Select Category"
+                  selection
+                  value={selectedCategory}
+                  options={categoryList.map(item => ({
+                    key: item,
+                    text: item,
+                    value: item
+                  }))}
+                  onChange={(e, data) => {
+                    this.setState({ selectedCategory: data.value }, () => {
+                      this.updateList();
+                    });
+                  }}
+                />
+              </div>
+              </div>
+              </Sidebar> 
+        <Bar className="chart"
         data={this.state.chartData}
         options={{
 
@@ -76,7 +202,7 @@ render() {
                 },
             title:{
                 display:this.props.displayTitle,
-                text:'"category": "Disease" ',
+                text: this.state.selectedCategory,
                 fontsize:25
             },
             legend:{
@@ -84,7 +210,9 @@ render() {
                 position:this.props.legendPosition
             }
         }}
-        />       
+        />    
+          
+        </Sidebar.Pushable>
     </div>
   );
 }
--- src/components/BarT.js
+++ src/components/BarT.js
@@ -1,6 +1,18 @@
 import React from 'react';
 import axios from 'axios';
 import { Bar} from 'react-chartjs-2';
+import {
+  Sidebar,
+  Segment,
+  Dropdown,
+  Button,
+  Menu,
+  Icon
+} from "semantic-ui-react";
+import "../css/appone.css";
+import "../css/bootstrap.min.css";
+import "../css/style.css";
+import "../css/bar.css";
 
 class BarT extends React.Component {
 
@@ -8,84 +20,199 @@ class BarT extends React.Component {
     super(props);
     this.state = {
       chartData: {},
+      master: {},
+      rankList: [],
+      selectedRank: "",
     }
+
+    this.importJSON = this.importJSON.bind(this);
+    this.updateList = this.updateList.bind(this);
   }
 
-  componentDidMount() {
+  toggleVisibility = () => this.setState({ visible: !this.state.visible });
 
-     axios('http://localhost:8080/visualization/barpie/taxonomy')
-      .then((response) => {
- 
-        const { taxonomyData } = response.data;
+   static defaultProps = {
+        displayTitle:true,
+        displayLegend:true,
+        legendPosition:'right',
+        
+    }
+  componentDidMount() {
+    this.importJSON()
+  }
+
+  importJSON() {
+    const ranks = {};
+    const master = {};
+    const rankList = [];
+
+    return fetch("http://localhost:8080/visualization/barpie/taxonomy")
+       .then(response => response.json())
+        .then(responseJson => {
+          this.setState(
+            {
+              TAXONOMY_DATA: responseJson.taxonomyData
+            },
+            () => {
+              this.state.TAXONOMY_DATA.forEach(d => {
+                const rank = d.rank;
+
+                ranks[rank] = true;
+                if (!master[rank]) {
+                  master[rank] = [];
+                }
+
+                master[rank].push(d);
+              });
+
+              // add ranks/categories to list
+              
+              for (let key in ranks) {
+                if (ranks.hasOwnProperty(key)) {
+                  rankList.push(key);
+                }
+              }
+             console.log("RankList", rankList);
+              //console.log('master', master)
+
+              this.setState(
+                {
+                  rankList,
+                  master,
+                  selectedRank: rankList[0]
+                  // selectedRank: "kingdom",
+                  // selectedCategory: "Technical term",
+                },
+                () => {
+                  this.updateList();
+                }
+              );
+            }
+          );
+        })
+  }
+
+  updateList(){
+    const { master, selectedRank } = this.state;
+    console.log(`master[${selectedRank}]`);
+    const Data = master[selectedRank];
+
         const chartData = {
-          labels: taxonomyData.map(k => k.taxonomyName),
+          labels: Data.map(k => k.taxonomyName),
           datasets: [
             {
               label: 'SpectraCount',
-              data: taxonomyData.map(d => d.noOfSpectra),
+              data: Data.map(d => d.noOfSpectra),
               backgroundColor: '#1c0549', 
             },
             {
               label: 'ProteinCount',
-              data: taxonomyData.map(p => p.noOfProtein),
+              data: Data.map(p => p.noOfProtein),
               backgroundColor: '#ff005a',
             },
             {
               label: 'PeptideCount',
-              data: taxonomyData.map(e => e.noOfPeptide),
+              data: Data.map(e => e.noOfPeptide),
               backgroundColor:  '#65cde3',
             }
-
-
           ]
         }
-
         this.setState({ chartData });
-      });
   }
   
 
 render() {
+  const {
+    visible,
+    rankList,
+    selectedRank
+  } = this.state;
+
   return (
-    <div className="App">
-       <Bar data={this.state.chartData} 
+    <div className="container">
+    <Sidebar.Pushable as={Segment} >
+          <Button
+            onClick={this.toggleVisibility}
+            icon
+            className="navbar-toggle"
+          >
+            <Icon name="align justify" />
+          </Button>
+          <Sidebar
+            as={Menu}
+            animation="overlay"
+            width="very wide"
+            direction="right"
+            visible={visible}
+            icon="labeled"
+            vertical
+            inverted
+          >
+            <Icon
+              className="close"
+              size="large"
+              onClick={this.toggleVisibility}
+            />
+            <div className="row mt-50" />
+
+            <div className="row mt-20">
+            <div className="col-sm-6">
+                <Dropdown
+                  placeholder="Select Rank"
+                  selection
+                  value={selectedRank}
+                  options={rankList.map(item => ({
+                    key: item,
+                    text: item,
+                    value: item
+                  }))}
+                  onChange={(e, data) => {
+                    this.setState({ selectedRank: data.value }, () => {
+                      this.updateList();
+                    });
+                  }}
+                />
+              </div>
+              </div>
+              </Sidebar> 
+        <Bar className="chart"
+        data={this.state.chartData}
         options={{
 
-          scales: {
-                   xAxes: [{
-                           display: true,
-                           scaleLabel: {
-                               display: true,
-                               ticks: {
-                                  autoSkip: false
-                                       },
-                               labelString: 'TaxynomyName'
-                           }
-                       }],
-                   yAxes: [{
-                           display: true,
-                           ticks: {
-                               beginAtZero: true,
-                               steps: 1000,
-                               stepValue: 500,
-                               max: 5000
-                           }
-                       }]
-               },
-           title:{
-               display:this.props.displayTitle,
-               text:'"RANK": "species" ',
-               fontsize:25
-           },
-           legend:{
-               display:this.props.displayLegend,
-               position:this.props.legendPosition
-           }
-       }} />
-
-      
-      
-
+           scales: {
+                    xAxes: [{
+                            display: true,
+                            scaleLabel: {
+                                display: true,
+                                ticks: {
+                                   autoSkip: false
+                                        },
+                                labelString: 'keywordName'
+                            }
+                        }],
+                    yAxes: [{
+                            display: true,
+                            ticks: {
+                                beginAtZero: true,
+                                steps: 1000,
+                                stepValue: 500,
+                                max: 5000
+                            }
+                        }]
+                },
+            title:{
+                display:this.props.displayTitle,
+                text: this.state.selectedRank,
+                fontsize:25
+            },
+            legend:{
+                display:this.props.displayLegend,
+                position:this.props.legendPosition
+            }
+        }}
+        />    
+          
+        </Sidebar.Pushable>
     </div>
   );
 }
